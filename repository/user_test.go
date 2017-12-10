0886ee0a2f886a5e672ce8b5d44aee3e2444b4be
tilaklodha <tilaklodha@gmail.com>
2017-12-11 01:21:11 +0530

    Adds test for 'GetUser'

--- repository/user_test.go
+++ repository/user_test.go
@@ -25,8 +25,30 @@ func (suite *UserRepositoryTestSuite) TestInsertUser() {
 	userOne := domain.NewUser("abc", "xyz", "city")
 	err := suite.repository.InsertUser(userOne)
 	assert.Nil(suite.T(), err)
+	suite.cleanUpDB()
+}
+
+func (suite *UserRepositoryTestSuite) TestGetUserWhenUserExists() {
+	suite.repository.db.MustExec("ALTER SEQUENCE users_id_seq RESTART 1;")
+	expectedUser := domain.NewUser("abc", "xyz", "city")
+	err := suite.repository.InsertUser(expectedUser)
+	assert.Nil(suite.T(), err)
+	observedUser, err := suite.repository.GetUser(1)
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), expectedUser, observedUser)
+	suite.cleanUpDB()
+}
+
+func (suite *UserRepositoryTestSuite) TestFailsGetUserWhenUserDoesNotExist() {
+	_, err := suite.repository.GetUser(1)
+	assert.NotNil(suite.T(), err)
 }
 
 func TestUserRepositorySuite(t *testing.T) {
 	suite.Run(t, new(UserRepositoryTestSuite))
+}
+
+func (suite *UserRepositoryTestSuite) cleanUpDB() {
+	db := appcontext.GetDB()
+	db.MustExec("DELETE from users")
 }
