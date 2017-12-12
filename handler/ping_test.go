72b46fe188b077dd65cfe12f2ea977fbce7fb2ae
tilaklodha <tilaklodha@gmail.com>
2017-12-12 08:40:07 +0530

    Fixes test for 'PingHandler'

--- handler/ping_test.go
+++ handler/ping_test.go
@@ -13,19 +13,19 @@ func TestPingHandler(t *testing.T) {
 	request, err := http.NewRequest("GET", "/ping", nil)
 	if err != nil {
 		log.Fatalf("Failed to create new request")
-
-		PingHandler(responseRecorder, request)
-		response := responseRecorder.Result()
-
-		if status := response.StatusCode; status != http.StatusOK {
-			t.Errorf("ping handler returned wrong status code: got %v want %v", status, http.StatusOK)
-		}
-
-		want := `"ping": "pong"`
-		body, _ := ioutil.ReadAll(response.Body)
-		got := string(body)
-		if got != want {
-			t.Errorf("ping handler returned unexpected body: got %v want %v", got, want)
-		}
+		return
 	}
+	PingHandler(responseRecorder, request)
+	response := responseRecorder.Result()
+	if status := response.StatusCode; status != http.StatusOK {
+		t.Errorf("ping handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	want := `"ping":"pong"`
+	body, _ := ioutil.ReadAll(response.Body)
+	got := string(body)
+	if got != want {
+		t.Errorf("ping handler returned unexpected body: got %v want %v", got, want)
+	}
+
 }
